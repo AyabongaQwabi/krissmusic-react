702c85defbb21170d1060b97a60e94a4e1c52d95
Aya <ayabonga@projectcodex.co>
2017-10-19 22:47:11 +0200

    Feat: Added play pause functionality

--- src/views/MusicControl/index.js
+++ src/views/MusicControl/index.js
@@ -3,17 +3,17 @@ import { connect } from 'react-redux';
 import * as R from 'ramda';
 import { PropTypes } from 'prop-types';
 import { Row, Col } from 'react-bootstrap';
-import { getImage, getSong} from '../../helpers/song';
 import PlayerControl from '../PlayerControl';
 import Tracklist from '../Tracklist';
 import '../../styles/musicControl.less';
 
 class MusicControl extends Component {
   render(){
+
     const { tracklist } = this.props;
-    const currentSong = R.head(tracklist);
-    const image = getImage(currentSong);
-    const song = getSong(currentSong);
+    const getCurrentSong = () => R.head(tracklist.toJS());
+    const currentSong = getCurrentSong();
+    const currentlyPlaying = !R.isNil(currentSong);
 
     return(
       <div>
@@ -21,7 +21,7 @@ class MusicControl extends Component {
         <Col xs={12} mdOffset={1} md={10} className='music-control'>
           <Col xs={12} md={12} className="panel panel-default dark">
             <div className="panel-body">
-             <PlayerControl image={image} song={song} />
+             {currentlyPlaying && <PlayerControl current={currentSong} tracklist={tracklist.toJS()}/>}
              <Tracklist tracks={tracklist.toJS()}/>
             </div>
           </Col>
--- src/views/PlayerControl/index.js
+++ src/views/PlayerControl/index.js
@@ -1,22 +1,77 @@
 import React, { Component } from 'react';
 import { PropTypes } from 'prop-types';
 import { Col, Glyphicon } from 'react-bootstrap';
+import { IMAGE_SOURCE } from '../../config/constants';
+import * as R from 'ramda';
 import '../../styles/player.less';
 
 class PlayerControl extends Component {
+  constructor(){
+    super();
+    this.OnPlay = this.OnPlay.bind(this);
+    this.OnNext = this.OnNext.bind(this);
+    this.OnPrevious = this.OnPrevious.bind(this);
+    this.state ={ playPauseGlyph: 'play', current:{} }
+  }
+
+  componentWillMount(){
+    this.setState({
+      current: this.props.current,
+    })
+  }
+
+  OnPlay(){
+    const { playPauseGlyph } = this.state;
+    const isPlaying = (playPauseGlyph === 'pause')
+    const updatedGlyph = isPlaying ? 'play' : 'pause';
+    this.setState({
+      playPauseGlyph : updatedGlyph,
+    });
+  }
+
+  OnPrevious(){
+    const { tracklist } = this.props;
+    const { current } = this.state;
+    const previousSongs = R.filter((song) => song.id < current.id , tracklist);
+    const toPlay = !R.isEmpty(previousSongs) ? R.head(previousSongs) : current;
+    this.setState({
+      current: toPlay,
+    })
+  }
+
+  OnNext(){
+    const { tracklist } = this.props;
+    const { current } = this.state;
+    const nextSongs = R.filter((song) => song.id > current.id , tracklist);
+    const toPlay = !R.isEmpty(nextSongs) ? R.head(nextSongs) : current;
+    this.setState({
+      current: toPlay,
+    })
+  }
+
   render() {
-    const { image } = this.props;
-    const imageSize = {height:"100px",width:"100px"}
+
+    const { image, artist, title } = this.state.current;
+    const imageSize = {height:"100px",width:"100px"};
+    const imageUrl = IMAGE_SOURCE.concat(image);
+
     return(
       <div>
-        <Col xs={2} md={2} mdOffset={2} className='player-control text-left'>
-            <Glyphicon glyph="backward" className='ctrl-btn backward'/>
-        </Col>
-        <Col xs={8} md={4} className=''>
-          <img src={image} alt='' style={imageSize} />
-        </Col>
-        <Col xs={2} md={2} className='player-control text-right'>
-            <Glyphicon glyph="forward" className='ctrl-btn forward'/>
+        <div style={{height:'100px'}}>
+          <Col xs={2} md={2} mdOffset={2} className='player-control text-left'>
+            <Glyphicon glyph="backward" className='ctrl-btn backward' onClick={this.OnPrevious} />
+          </Col>
+          <Col xs={8} md={4} className='play-pause'>
+            <img src={imageUrl} alt={artist} style={imageSize} />
+            <Glyphicon glyph={this.state.playPauseGlyph} className='control' onClick={this.OnPlay} />
+          </Col>
+          <Col xs={2} md={2} className='player-control text-right'>
+            <Glyphicon glyph="forward" className='ctrl-btn forward' onClick={this.OnNext} />
+          </Col>
+        </div>
+        <Col xs={12} md={12}>
+          <small>Now Playing </small><br />
+          <small> {artist} - {title} </small>
         </Col>
       </div>
     )
@@ -24,8 +79,13 @@ class PlayerControl extends Component {
 }
 
 PlayerControl.propTypes = {
-  image:PropTypes.string.isRequired,
-  song:PropTypes.string.isRequired,
+  current: PropTypes.shape({
+    artist: PropTypes.string,
+    title: PropTypes.string,
+    song: PropTypes.string,
+    image: PropTypes.string,
+  }),
+  tracklist: PropTypes.array.isRequired,
 }
 
 export default PlayerControl;
